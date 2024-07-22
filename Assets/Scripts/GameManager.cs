5faacc9527ae15daea025430fd392581fbd8ef50
Julieed-971 <julie.dedieu1@gmail.com>
2024-07-22 16:13:10 +0200

    Building the BakeryBreaker

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int level = 1;
+    public int score = 0;
+    public int lives = 3;
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        this.score = 0;
+        this.lives = 3;
+
+        LoadLevel(1);
+    }
+
+    private void LoadLevel(int level)
+    {
+        this.level = level;
+
+        SceneManager.LoadScene("Level" + level);
     }
 }
